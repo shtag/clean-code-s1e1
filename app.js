237e0d2bc00b4b1ac7cf5a9103573530ad024e19
shtag <vasyakanishchev@gmail.com>
2022-10-28 23:34:23 +0300

    fix: finctional app

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ var createNewTaskElement=function(taskString){
     editInput.className="task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="edit-task";
 
     deleteButton.className="delete-task";
     deleteButtonImg.src='./remove.svg';
